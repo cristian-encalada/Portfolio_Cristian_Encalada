b60800ed8f1a72f32f04cd5327e4ff98d7813eeb
cristian-encalada <cris.encalada.camargo@gmail.com>
2021-02-16 17:05:12 -0400

    Testimonial section completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,7 +77,26 @@ function activePortfolio(){
 linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
 
 /*===== SWIPER CAROUSEL =====*/ 
-
+const swiper = new Swiper('.testimonial__container', {
+    // Optional parameters
+    spaceBetween: 16,
+    loop: true,
+    grabCursor: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        }
+    }
+  });
 
 /*===== GSAP ANIMATION =====*/ 
 
