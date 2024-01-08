054a2da510f82a0532c3509643159228460782f5
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2024-01-08 14:24:33 -0300

    Fox model, Contact page and Alert hook done

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import Loader from "./Loader";
 import Navbar from "./Navbar";
 import HomeInfo from "./HomeInfo";
+import Alert from "./Alert";
 
 export {
+    Alert,
     Loader,
     Navbar,
     HomeInfo
